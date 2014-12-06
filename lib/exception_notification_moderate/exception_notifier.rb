4111f7639448ebade7bd86cab3b1a6bd144c5401
ryonext <ryonext.s@gmail.com>
2014-12-06 18:21:52 +0900

    Rescue redis socket error.

--- lib/exception_notification_moderate/exception_notifier.rb
+++ lib/exception_notification_moderate/exception_notifier.rb
@@ -7,20 +7,15 @@ module ExceptionNotifier
 
     def notify_exception(exception, options={})
       ex_key = exception_key(exception)
-      begin
-        val = redis.get(ex_key)
-      rescue Redis::CannotConnectError
-      end
+
+      val = execute_with_rescue_error { redis.get(ex_key) }
       if val && val.to_i + 60 > Time.now.to_i
         # Write log and return.
         ExceptionNotificationModerate.logger.info(exception)
         return false
       end
 
-      begin
-        redis.set(ex_key, Time.now.to_i)
-      rescue Redis::CannotConnectError
-      end
+      execute_with_rescue_error { redis.set(ex_key, Time.now.to_i) }
       original_notify_exception(exception, options)
     end
 
@@ -28,6 +23,11 @@ module ExceptionNotifier
 
     def exception_key(exception)
       "#{exception.to_s}_#{exception.message}"
+    end
+
+    def execute_with_rescue_error
+      yield
+    rescue Redis::CannotConnectError, SocketError
     end
 
     def redis
